74f0ca213f60323ccdfd3fc8cfd7d7b2a4ce0380
HK5YK <yokoi0503pandorabox@gmail.com>
2021-03-20 02:55:42 +0000

    Initial commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'd1c8d1c5df9ae81a25dd83446f2b130036be8d584f21c6676f032b09a3bb3777d6dd21415917f7ecf9b3c7907b0e235e646b19feff79a6626084c38d3fc2a64a'
